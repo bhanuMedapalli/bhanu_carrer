4ad29f7d59f63a698cf1cfdea5c2273f3dd463cf
bhanuMedapalli <154443681+bhanuMedapalli@users.noreply.github.com>
2024-01-30 13:01:24 +0000

    new file added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, jsonify
 
 app = Flask (__name__)
 JOBS=[
@@ -13,6 +13,12 @@ def name():
   return render_template('home.html',
                          jobs=JOBS,
                          company_name='Bhanu')
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
+
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
